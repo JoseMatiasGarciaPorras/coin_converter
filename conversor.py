246c5e0abb9799bce92a408c8b32ddd99c9af9a5
José Matías García Porras <jmgarciaporras@gmail.com>
2021-01-10 19:54:57 +0100

    Functional change €, $, L

--- conversor.py
+++ conversor.py
@@ -1,10 +1,45 @@
-pesos = input('¿Cuántos pesos colombianos tienes? ')
+''' pesos = input('¿Cuántos pesos colombianos tienes? ')
 pesos = float(pesos)
 valor_dolar = 3875
 dolares = pesos/valor_dolar
 dolares = round(dolares, 2)
 dolares = str(dolares)
-print('Tienes {}$'.format(dolares))
+print('Tienes {}$'.format(dolares)) '''
+
+
+def change(coin1, coin2, mount):
+    result = 0
+    if coin1 == 1 and coin2 == 1:
+        result = mount * 1
+        print('{} dolares equivalen a {} dolares'.format(mount, result))
+    elif coin1 == 1 and coin2 == 2:
+        result = mount * 0.82
+        print('{} dolares equivalen a {} euros'.format(mount, result))
+    elif coin1 == 1 and coin2 == 3:
+        result = mount * 0.74
+        print('{} dolares equivalen a {} libras'.format(mount, result))
+    elif coin1 == 2 and coin2 == 1:
+        result = mount * 1.22
+        print('{} euros equivalen a {} dolares'.format(mount, result))
+    elif coin1 == 2 and coin2 == 2:
+        result = mount * 1
+        print('{} euros equivalen a {} euros'.format(mount, result))
+    elif coin1 == 2 and coin2 == 3:
+        result = mount * 0.90
+        print('{} euros equivalen a {} libras'.format(mount, result))
+    elif coin1 == 3 and coin2 == 1:
+        result = mount * 1.36
+        print('{} libras equivalen a {} dolares'.format(mount, result))
+    elif coin1 == 3 and coin2 == 2:
+        result = mount * 1.11
+        print('{} libras equivalen a {} euros'.format(mount, result))
+    elif coin1 == 3 and coin2 == 3:
+        result = mount * 1
+        print('{} libras equivalen a {} libras'.format(mount, result))
+    else:
+        print('Solo se admiten los valores prefijados')
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import conversor
-if __name__ == '__main__':
 
+if __name__ == '__main__':
+    try:
+        print('********** COIN CONVERTER **********')
+        initial_coin = int(input('''
+        Ingresa el índice de la moneda que quieres convertir: 
+            [1] Dolar.
+            [2] Euro.
+            [3] Libra.
+        Elección: '''))
+        print('*******************************')
+        final_coin = int(input('''
+        Ingresa el índice de la moneda que quieres obtener:
+            [1] Dolar.
+            [2] Euro.
+            [3] Libra.
+        Elección: '''))
+        print('*******************************')
+        mount = int(input('Ingresa la cantidad a convertir: '))
+        conversor.change(initial_coin, final_coin, mount)
+    except:
+        print('********** ERROR **********')
+        print('Solo se admiten valores numéricos')
